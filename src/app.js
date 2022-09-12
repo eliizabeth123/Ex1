4007d999ab96f7e8ea25de361efdd6ff04b10fcf
“elmand” <elmand@ttu.ee>
2022-09-12 17:59:18 +0300

    Deploying to Netlify

---
+++ src/app.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const serverless = require('serverless-http')
+const app = express()
+const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hello ' + req.query.name)
+})
+
+router.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+app.use('/.netlify/functions/app',router);
+
+module.exports.handler = serverless(app);
