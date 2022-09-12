e166bd0a7d6ce0c2a5260583458ef574f9628511
“elmand” <elmand@ttu.ee>
2022-09-12 19:24:32 +0300

    bug fix

--- src/api.js
+++ src/api.js
@@ -1,18 +1,15 @@
 const express = require('express')
 const serverless = require('serverless-http')
 const app = express()
-const router = express.Router();
 
-router.get('/', (req, res) => {
+app.get('/.netlify/functions/api', (req, res) => {
   res.send('Hello ' + req.query.name)
 })
 
-router.listen(3000, () => {
+
+app.listen(3000, () => {
   console.log(`Example app listening on port 3000`)
 })
-
-
-app.use('/.netlify/functions/api', router);
 
 module.exports = app;
 module.exports.handler = serverless(app);
